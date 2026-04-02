2ab93c1bef4846e95b553223c7e5b890ab603a9e
jiluoQAQ <376148946@qq.com>
2026-04-02 13:06:35 +0800

    图片资源下载配置

--- RocomUID/rocom_resource/__init__.py
+++ RocomUID/rocom_resource/__init__.py
@@ -7,7 +7,7 @@ from ..utils.resource.download_all_file import check_use
 sv_rc_download_config = SV('rc下载资源', pm=1)
 
 
-@sv_rc_download_config.on_fullmatch('rc下载全部资源')
+@sv_rc_download_config.on_fullmatch('下载全部资源')
 async def send_download_resource_msg(bot: Bot, ev: Event):
     await bot.send('rc正在开始下载~可能需要较久的时间!')
     im = await check_use()
