c75525509accc9987a512f92266c83978537d2ed
jiluoQAQ <376148946@qq.com>
2026-04-20 16:07:20 +0800

    fix bug

--- RocomUID/rocom_merchant/__init__.py
+++ RocomUID/rocom_merchant/__init__.py
@@ -7,7 +7,7 @@ from gsuid_core.sv import SV
 from gsuid_core.bot import Bot
 from gsuid_core.models import Event
 from datetime import datetime, timedelta
-from ..utils.rocom_api import wegame_api
+from ..utils.rocom_api import wegame_api, cocom_co_api
 from gsuid_core.subscribe import gs_subscribe
 from gsuid_core.aps import scheduler
 from ..utils.error_reply import prefix as P
@@ -18,32 +18,26 @@ sv_merchant = SV('rc远行商人事件', priority=5)
 async def get_merchant_info_list(bot: Bot, ev: Event):
     merchant_info = await wegame_api.get_merchant_info(refresh=True)
     if len(merchant_info) == 0:
-        return bot.send(f"远行商人商品未刷新\n可输入[{P}开启远行商人]订阅远行商人商品信息推送", at_sender=True)
+        return await bot.send(f"远行商人商品未刷新\n可输入[{P}开启远行商人]订阅远行商人商品信息推送", at_sender=True)
     mesg = "当前商人商品："
     for item in merchant_info:
         mesg += f"\n{item['name']} 结束时间：{item['endtime']}"
     mesg += f"\n可输入[{P}开启远行商人]订阅远行商人商品信息推送"
     await bot.send(mesg, at_sender=True)
 
-# @sv_merchant.on_command(('远行商人推送测试'))
-# async def get_merchant_info_list_cs(bot: Bot, ev: Event):
-    # merchant_info = await wegame_api.get_merchant_info(refresh=True)
-    # mesg = "远行商人商品刷新了："
-    # for item in merchant_info:
-        # mesg += f"\n{item['name']} 结束时间：{item['endtime']}"
-    # datas = await gs_subscribe.get_subscribe('[洛克王国] 远行商人')
-    # for data in datas:
-        # await data.send(mesg)
-
 # 每日定点执行远行商人推送
-@scheduler.scheduled_job('cron', hour ='*', minute='05')
+@scheduler.scheduled_job('cron', hour ='*')
 async def refresh_merchant_info():
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
     this_hour = now.hour
     if this_hour not in [8, 12, 16, 20]:
         return
-    await asyncio.sleep(10)
-    merchant_info = await wegame_api.get_merchant_info(refresh=True)
+    merchant_info= []
+    jishu = 0
+    while len(merchant_info) == 0 and jishu < 20:
+        await asyncio.sleep(5)
+        jishu = jishu + 1
+        merchant_info = await wegame_api.get_merchant_info(refresh=True)
     mesg = "远行商人商品刷新了："
     for item in merchant_info:
         mesg += f"\n{item['name']} 结束时间：{item['endtime']}"
